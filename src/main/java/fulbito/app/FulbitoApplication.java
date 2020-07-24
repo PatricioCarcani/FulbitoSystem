b8a42a4a25a288086d259375cfa834f73284a02c
PatricioCarcani <carcani.patricio@gmail.com>
2020-07-23 21:26:19 -0300

    Creado projecto con springboot conectado a PostreSQL local

---
+++ src/main/java/fulbito/app/FulbitoApplication.java
@@ -0,0 +1,18 @@
+package fulbito.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulbitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FulbitoApplication.class, args);
+		
+
+        
+
+		
+	}
+
+}
---
+++ src/test/java/grupo/uno/FulbitoApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo.uno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FulbitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
