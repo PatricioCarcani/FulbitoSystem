a4de80d0e2df24f71b987f219b257ad55adc74a2
PatricioCarcani <carcani.patricio@gmail.com>
2020-07-28 19:54:06 -0300

    Agregadas clasesde pull request

--- src/main/java/fulbito/app/modelos/servicios/IPersonaService.java
+++ src/main/java/fulbito/app/modelos/servicios/IPersonaService.java
@@ -5,15 +5,20 @@ import java.util.List;
 import fulbito.app.modelos.entidades.Persona;
 
 public interface IPersonaService {
-	
+
+	// SELECT...
 	public List<Persona> listar();
 	
+	// SELECT... WHERE ID=?
 	public Persona listarPorId(Long id);
 	
+	// INSERT...
 	public Persona guardar(Persona persona);
 	
+	// UPDATE...
 	public Persona modificar(Persona persona, Long id);
 	
+	// DELETE...
 	public void borrar(Long id);
 
 }
